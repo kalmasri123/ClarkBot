bebbd2a56b445b64e7c93680217d71acabe8a9a2
kalmasri123 <kalmasri@me.com>
2018-10-08 19:20:05 -0400

    Update scorekeeping.js

--- scorekeeping.js
+++ scorekeeping.js
@@ -6,6 +6,7 @@ var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 
 
+
 var mongodbDatabase = 'world';
 
 // connect string for mongodb server running locally, connecting to a database called test

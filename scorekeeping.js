fcc9a2479b5bf6ba044a997eccfd7bf2e0bcd263
kalmasri123 <kalmasri@me.com>
2018-10-08 16:56:16 -0400

    Update scorekeeping.js

--- scorekeeping.js
+++ scorekeeping.js
@@ -1,34 +1,54 @@
 var blessings= require('./blessings.json')
 
 var fs= require("fs")
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
 
 module.exports={
 
 addBless:function(id){
 
+MongoClient.connect(url, function(err, db) {
 
-if(!blessings[id]){
+  if (err) throw err;
+  var dbo = db.db("mydb");
 
-  blessings[id]=0;
 
+
+  dbo.collection("blessingCounter").find({user:id}).toArray(function(err, blessings) {
+
+      if (err) throw err;
+
+
+
+
+if(blessings.length==0){
+var myobj={user:id,score:0}
+
+  dbo.collection("blessingCounter").insertOne(myobj, function(err, res) {
+    if (err) throw err;
+
+    console.log("1 document inserted");
+})
 
 
 }
-blessings[id]+=1;
-console.log(blessings)
 
-fs.writeFile("./blessings.json", JSON.stringify(blessings), (err) => {
-    if (err) {
-        console.error(err);
-        return;
-    };
-    console.log("File has been created");
-});
+console.log(blessings[0])
+  var newvalues = { $set: { score:blessings[0].score+1 } };
+dbo.collection("blessingCounter").updateOne({user:id}, newvalues, function(err, res) {
+    if (err) throw err;
 
+    console.log("1 document updated");
 
+db.close()
+
+})
+
+})
+
+})
 
 }
-
-
-
 }

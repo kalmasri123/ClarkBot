5f591e250cfa92659a4f6e1e9bf6207278a86b84
kalmasri123 <kalmasri@me.com>
2018-10-09 14:50:56 -0400

    fe

--- app.js
+++ app.js
@@ -18,7 +18,10 @@ var scorekeeping=require('./scorekeeping')
 
   client.on('presenceUpdate',(oldMember,newMember)=>{
 if(newMember.presence.game){
+  if(newMember.presence.game.name=="Fortnite"){
 oldMember.sendMessage("**Ten Minutes of no Fortnite will lead to Better grades on your Pop Test**")
+
+}
 console.log(newMember.presence.game.name)
 
 

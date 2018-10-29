2e6d3daaa1ba5d93b94600f5654b1cb88c479d3d
kalmasri123 <kalmasri@me.com>
2018-10-29 13:10:43 -0400

    Update app.js

--- app.js
+++ app.js
@@ -134,8 +134,8 @@ firstReq()
 
 }else
 {
-message.channel.send(JSON.parse(data)[0].data.children[0].data.title)
-message.channel.send( JSON.parse(data)[0].data.children[0].data.url)
+message.channel.send("**"+JSON.parse(data)[0].data.children[0].data.title+"**")
+message.channel.send("**"+JSON.parse(data)[0].data.children[0].data.url+"**")
 console.log("Hello")
 }
 

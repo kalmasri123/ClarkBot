baee49dea14fac1530914591444d241b23ab0ed2
kalmasri123 <kalmasri@me.com>
2019-01-07 22:11:00 -0500

    wdw

--- server.js
+++ server.js
@@ -5,25 +5,34 @@ const {
 } = require('querystring');
 
 const eventHandler = require("./eventHandler.js")
-//const bot = require('./app.js')
+const bot = require('./app.js')
 const server = express()
 var path = require('path')
 var bodyParser = require('body-parser');
 server.use(bodyParser.json()); // support json encoded bodies
-server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 
-server.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/index.html'))
+server.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'))
 })
 const data = {
-  'KEY':'KUDEEMDOHOMWOK'
+  'KEY': 'KUDEEMDOHOMWOK'
 }
 
-server.post('/',function(req,res)
-{
-const post = req.body;
-console.log(post)
+server.post('/', function(req, res) {
+  const post = req.body;
+  if (post.KEY == data.KEY) {
+    eventHandler.em.emit('globalMessage', post.MSG)
+    res.end("GRANTED")
 
+  } else {
+    res.end("DENIED")
+  }
+  res.end(post.KEY)
 })
+
+
 server.listen(3000);

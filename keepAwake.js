f273db7c355e3bbf30177caff7351049adac40a2
kalmasri123 <kalmasri@me.com>
2019-01-13 09:58:47 -0500

    Update keepAwake.js

--- keepAwake.js
+++ keepAwake.js
@@ -1,4 +1,4 @@
 var http = require("https");
 setInterval(function() {
-    http.get("https:localhost/");
+    http.get("https://clarkbot12.herokuapp.com/");
 }, 300000); // every 5 minutes (300000)
